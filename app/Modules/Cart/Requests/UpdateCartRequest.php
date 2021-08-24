9cb74344a9d82f14902aafc791136f2376bbf449
Mohammad Ali <mdali2016.227@gmail.com>
2021-08-25 00:21:59 +0600

    [Change] refactored Requests inside Cart Module

--- app/Modules/Cart/Requests/UpdateCartRequest.php
+++ app/Modules/Cart/Requests/UpdateCartRequest.php
@@ -2,26 +2,17 @@
 
 namespace App\Modules\Cart\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
 
-class UpdateCartRequest extends FormRequest
+class UpdateCartRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() : array
     {
         return [
             'id' => 'required|array',
