4407622a3e91a28102639c23d815a5c3a8a0c1be
Mohammad Ali <mdali2016.227@gmail.com>
2021-08-26 22:43:59 +0600

    [Change] refactored Requests inside Contact Module

--- app/Modules/Contact/Requests/StoreContactMessageRequest.php
+++ app/Modules/Contact/Requests/StoreContactMessageRequest.php
@@ -2,26 +2,16 @@
 
 namespace App\Modules\Contact\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
 
-class StoreContactMessageRequest extends FormRequest
+class StoreContactMessageRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() : array
     {
         return [
             'to' => 'required|string',
