f2ac8962a63eca72abc60523c8a166fc296d1728
Mohammad Ali <mdali2016.227@gmail.com>
2021-08-30 11:43:39 +0600

    [Change] refactored FAQService.php inside FAQ Module

--- app/Modules/FAQ/Services/FAQService.php
+++ app/Modules/FAQ/Services/FAQService.php
@@ -3,14 +3,13 @@
 
 namespace App\Modules\FAQ\Services;
 
+use App\Http\Services\ResponseService;
 use App\Modules\FAQ\Repositories\FAQRepository;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-class FAQService
+class FAQService extends ResponseService
 {
-    private $errorMessage;
-    private $errorResponse;
     private $faqRepository;
 
     /**
@@ -20,22 +19,13 @@ class FAQService
     public function __construct(FAQRepository $faqRepository)
     {
         $this->faqRepository = $faqRepository;
-        $this->errorMessage = __('Something went wrong');
-        $this->errorResponse = [
-            'success' => false,
-            'message' => $this->errorMessage,
-            'data' => [],
-            'webResponse' => [
-                'dismiss' => $this->errorMessage,
-            ],
-        ];
     }
 
     /**
      * @param $request
      * @return array
      */
-    public function saveSettings($request)
+    public function saveSettings($request): array
     {
         try {
             DB::beginTransaction();
@@ -46,25 +36,12 @@ class FAQService
                 $this->faqRepository->updateOrCreate(['slug' => $key], ['user_id' => Auth::user()->id,'value' => $value]);
             }
             DB::commit();
-            return [
-                'success' =>  true,
-                'message' => __('FAQ has been updated successfully'),
-                'data' => null,
-                'webResponse' => [
-                    'success' => __('FAQ has been updated successfully'),
-                ],
-            ];
+
+            return $this->response()->success('FAQ has been updated successfully');
         } catch (\Exception $exception) {
             DB::rollBack();
 
-            return [
-                'success' =>  false,
-                'message' => __('Something went wrong. Please try again.') . $exception->getMessage(),
-                'data' => null,
-                'webResponse' => [
-                    'dismiss' => __('Something went wrong. Please try again.') . $exception->getMessage(),
-                ],
-            ];
+            return $this->response()->error($exception->getMessage());
         }
     }
 }
