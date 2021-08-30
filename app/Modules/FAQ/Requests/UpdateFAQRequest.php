e577101332da5b1861c8c17b8e22ca6644416814
Mohammad Ali <mdali2016.227@gmail.com>
2021-08-30 11:48:43 +0600

    [Change] refactored Requests inside FAQ Module

--- app/Modules/FAQ/Requests/UpdateFAQRequest.php
+++ app/Modules/FAQ/Requests/UpdateFAQRequest.php
@@ -2,42 +2,32 @@
 
 namespace App\Modules\FAQ\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
 
-class UpdateFAQRequest extends FormRequest
+class UpdateFAQRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() : array
     {
         return [
-            'about_us' => isset($this->about_us) ? 'required|string' : '',
-            'about_us_bn' => isset($this->about_us_bn) ? 'required|string' : '',
-            'offerings' => isset($this->offerings) ? 'required|string' : '',
-            'offerings_bn' => isset($this->offerings_bn) ? 'required|string' : '',
-            'payment_information' => isset($this->payment_information) ? 'required|string' : '',
-            'payment_information_bn' => isset($this->payment_information_bn) ? 'required|string' : '',
-            'delivery_information' => isset($this->delivery_information) ? 'required|string' : '',
-            'delivery_information_bn' => isset($this->delivery_information_bn) ? 'required|string' : '',
-            'order_guideline' => isset($this->order_guideline) ? 'required|string' : '',
-            'order_guideline_bn' => isset($this->order_guideline_bn) ? 'required|string' : '',
-            'terms_and_conditions' => isset($this->terms_and_conditions) ? 'required|string' : '',
-            'terms_and_conditions_bn' => isset($this->terms_and_conditions_bn) ? 'required|string' : '',
-            'privacy_policy' => isset($this->privacy_policy) ? 'required|string' : '',
-            'privacy_policy_bn' => isset($this->privacy_policy_bn) ? 'required|string' : '',
+            'about_us' => isset($this->about_us) ? 'string' : '',
+            'about_us_bn' => isset($this->about_us_bn) ? 'string' : '',
+            'offerings' => isset($this->offerings) ? 'string' : '',
+            'offerings_bn' => isset($this->offerings_bn) ? 'string' : '',
+            'payment_information' => isset($this->payment_information) ? 'string' : '',
+            'payment_information_bn' => isset($this->payment_information_bn) ? 'string' : '',
+            'delivery_information' => isset($this->delivery_information) ? 'string' : '',
+            'delivery_information_bn' => isset($this->delivery_information_bn) ? 'string' : '',
+            'order_guideline' => isset($this->order_guideline) ? 'string' : '',
+            'order_guideline_bn' => isset($this->order_guideline_bn) ? 'string' : '',
+            'terms_and_conditions' => isset($this->terms_and_conditions) ? 'string' : '',
+            'terms_and_conditions_bn' => isset($this->terms_and_conditions_bn) ? 'string' : '',
+            'privacy_policy' => isset($this->privacy_policy) ? 'string' : '',
+            'privacy_policy_bn' => isset($this->privacy_policy_bn) ? 'string' : '',
             'image' => isset($this->image) ? 'mimes:jpeg,jpg,JPG,png,PNG,gif|max:4000' : '',
         ];
     }
