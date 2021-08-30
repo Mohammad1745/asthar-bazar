dfdd299315824676ffc1054138b7763383c14da9
Mohammad Ali <mdali2016.227@gmail.com>
2021-08-30 12:12:55 +0600

    [Change] refactored Requests inside News Module.

--- app/Modules/News/Requests/StoreNewsRequest.php
+++ app/Modules/News/Requests/StoreNewsRequest.php
@@ -2,26 +2,17 @@
 
 namespace App\Modules\News\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 
-class StoreNewsRequest extends FormRequest
+use App\Http\Requests\Request;
+
+class StoreNewsRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() : array
     {
         return [
             'content' => 'required|string',
--- app/Modules/News/Requests/UpdateNewsRequest.php
+++ app/Modules/News/Requests/UpdateNewsRequest.php
@@ -2,26 +2,16 @@
 
 namespace App\Modules\News\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
 
-class UpdateNewsRequest extends FormRequest
+class UpdateNewsRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() : array
     {
         return [
             'id' => 'required|numeric',
