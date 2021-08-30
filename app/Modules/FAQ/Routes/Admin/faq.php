1d312c43d351bb4a349418c278ec869bcc5eda19
Mohammad Ali <mdali2016.227@gmail.com>
2021-08-30 11:39:00 +0600

    [Change] refactored Routes inside FAQ Module

--- app/Modules/FAQ/Routes/Admin/faq.php
+++ app/Modules/FAQ/Routes/Admin/faq.php
@@ -1,3 +1,5 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('faq/{faq}', "FAQController@faq")->name('admin.faq');
--- app/Modules/FAQ/Routes/SuperAdmin/faq.php
+++ app/Modules/FAQ/Routes/SuperAdmin/faq.php
@@ -1,4 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('faq', "FAQController@faq")->name('superAdmin.faq');
 Route::post('faq/update', "FAQController@update")->name('superAdmin.faq.update');
