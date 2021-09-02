43922048e65c5e587d2cbe14fc063eaabd381c6e
Mohammad Ali <mdali2016.227@gmail.com>
2021-09-02 10:57:48 +0600

    [Change] refactored Routes inside Profile Module

--- app/Modules/Profile/Routes/Admin/profile.php
+++ app/Modules/Profile/Routes/Admin/profile.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('profile', "ProfileController@profile")->name('admin.profile');
 Route::post('profile/update', "ProfileController@update")->name('admin.profile.update');
 Route::post('profile/update-password', "ProfileController@updatePassword")->name('admin.profile.updatePassword');
--- app/Modules/Profile/Routes/User/Web/profile.php
+++ app/Modules/Profile/Routes/User/Web/profile.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('profile', "ProfileController@profile")->name('profile');
 Route::post('profile/update', "ProfileController@update")->name('profile.update');
 Route::post('profile/update-password', "ProfileController@updatePassword")->name('profile.updatePassword');
