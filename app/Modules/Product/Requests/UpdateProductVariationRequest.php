a910ee8731a91dbffe6d32163db9a6be25db1d8f
Mohammad Ali <mdali2016.227@gmail.com>
2021-09-02 10:56:30 +0600

    [Change] refactored Requests inside Product Module

--- app/Modules/Product/Requests/UpdateProductVariationRequest.php
+++ app/Modules/Product/Requests/UpdateProductVariationRequest.php
@@ -2,26 +2,16 @@
 
 namespace App\Modules\Product\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
 
-class UpdateProductVariationRequest extends FormRequest
+class UpdateProductVariationRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() :array
     {
         return [
             'id' => 'required|numeric',
