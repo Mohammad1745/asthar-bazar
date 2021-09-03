4065e58a045d204484406e8f948cb71e3e415bc8
Mohammad Ali <mdali2016.227@gmail.com>
2021-09-03 13:36:31 +0600

    [Change] refactored Requests inside Profile Module

--- app/Modules/Profile/Requests/UpdateProfileRequestRequest.php
+++ app/Modules/Profile/Requests/UpdateProfileRequestRequest.php
@@ -2,26 +2,16 @@
 
 namespace App\Modules\Profile\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
 
-class UpdateProfileRequestRequest extends FormRequest
+class UpdateProfileRequestRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() :array
     {
         return [
             'first_name' => 'required|string',
